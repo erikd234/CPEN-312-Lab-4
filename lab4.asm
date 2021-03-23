843229ffc6ef1677aedf321a904a23d163804d52
Erik Dahl <erikd234@gmail.com>
2021-03-22 23:35:46 -0700

    Finishing scroll right and left
    Now student number scrolls in both directions.

--- lab4.asm
+++ lab4.asm
@@ -17,6 +17,9 @@ mov r6, #L_6
 mov r5, #L_5
 mov r4, #L_4
 mov r3, #L_3
+mov r2, #L_2 ; 7
+mov r1, #L_1 ; 5
+mov r0, #L_0 ; 7
 
 ;; jumping to the main loop of the program
  ljmp forever_loop;
@@ -33,6 +36,8 @@ update_displays:
 	mov hex2, a
 	mov a, r3
 	mov hex1, a
+	mov a, r2
+	mov hex0, a
 	ret
 
 ; this subroutine shifts all the data in the registers over to the right
@@ -43,35 +48,41 @@ shift_registers_right:
 	xch a, r5 ; r5 = 2 a = 9
 	xch a, r4 ; r4 = 9 a = 4
 	xch a, r3 ; r3 = 4, a = 6
+	xch a, r2 ; r3 = 4, a = 6
+	xch a, r1
+	xch a, r0
 	mov r7, a
 	ret
 	
 shift_registers_left:
 	clr a
-	mov a, r3 ; a = 8
+	mov a, r0 ; r3 = 4, a = 6
+	xch a, r1
+	xch a, r2
+	xch a, r3 ; a = 8
 	xch a, r4 ; r6 = 8 a = 2
 	xch a, r5 ; r5 = 2 a = 9
 	xch a, r6 ; r4 = 9 a = 4
-	xch a, r3 ; r3 = 4, a = 6
-	mov r7, a
+	xch a, r7 ; r3 = 4, a = 6
+	mov r0, a
 	ret
 	
 	
 ; this is the subroutine that causes a delay in the program
 wait:
-    mov R2, #180  ; 90 is 5AH
-L3: mov R1, #250 ; 250 is FAH 
-L2: mov R0, #250
-L1: djnz R0, L1  ; 3 machine cycles-> 3*30ns*250=22.5us
-    djnz R1, L2  ; 22.5us*250=5.625ms
-    djnz R2, L3  ; 5.625ms*90=0.506s (approximately)
+    mov 0BH, #180  ; 90 is 5AH
+L3: mov 0AH, #250 ; 250 is FAH 
+L2: mov 9H, #250
+L1: djnz 9H, L1  ; 3 machine cycles-> 3*30ns*250=22.5us
+    djnz 0AH, L2  ; 22.5us*250=5.625ms
+    djnz 0BH, L3  ; 5.625ms*90=0.506s (approximately)
 	ret
 	
 
 forever_loop:
 	lcall update_displays
 	lcall wait
-	lcall shift_registers_right
+	lcall shift_registers_left
 	ljmp forever_loop 
 
 
